ce8a2e0e5b2434dabab47fd1a78542b2b64041bf
GabrielRivera07 <rivehect40@gmail.com>
2025-11-14 14:54:30 -0600

    fix prisma env

--- server/prisma.js
+++ server/prisma.js
@@ -1,15 +1,10 @@
 const { PrismaClient } = require('@prisma/client');
 
-console.log("👉 NODE_ENV:", process.env.NODE_ENV);
-console.log("👉 PRISMA_DATABASE_URL existe?:", !!process.env.PRISMA_DATABASE_URL);
-
 let prisma;
 
-// En producción (Vercel Serverless)
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // En desarrollo: hot-reload crea muchas conexiones
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
