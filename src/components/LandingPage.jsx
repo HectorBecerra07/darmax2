2362d51b1f35e195139d78479305d15baa4fc2c4
GarkarAXO <e.axel12@gmail.com>
2025-11-21 12:21:45 -0600

    Change(banner, landingpage): new size for landingpage and new banner

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -23,26 +23,28 @@ export default function LandingPage() {
 
       {/* HERO */}
       <section
-        className=" text-white px-4 md:px-50 aspect-[2.5/1] md:aspect-[3.5/1]"
+        className="text-white px-4 md:px-8 pt-24 pb-16 md:pt-32 md:pb-24 lg:aspect-[3.5/1] lg:pt-0 lg:pb-0"
         style={{
-          backgroundImage: 'url("/img/fondos.jpg")',
+          backgroundImage: 'url("/img/banner.png")',
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        <div className="flex flex-col md:flex-row items-center justify-between gap-8 w-full max-w-7xl mx-auto h-full">
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-4 md:gap-8 w-full max-w-7xl mx-auto lg:h-full">
           <div className="max-w-xl text-center md:text-left flex flex-col items-center md:items-start">
-            <h2 className="text-3xl md:text-5xl font-bold leading-tight mb-4">
+            <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-black tracking-tight leading-tight mt-0 md:mt-4 mb-4 md:mb-6">
               EMPRENDE TU{" "}
-              <span className="text-accent -skew-x-6 inline-block">NEGOCIO</span>
+              <span className="text-accent">
+                NEGOCIO
+              </span>
             </h2>
-            <p className="text-[#ccff00] text-sm md:text-lg mb-6">
-              DA EL PRIMER PASO A LA TU LIBERTAD FINANCIERA Y <br></br>
-              LLEVA TU EMPRENDIEMITO AL SIGUIENTE NIVEL CON 
+            <p className="text-base md:text-lg lg:text-lg text-white leading-relaxed mb-6 md:mb-8">
+              Da el primer paso a tu libertad financiera y <br></br>
+              lleva tu emprendimiento al siguiente nivel con 
             </p>
             <button
               onClick={() => scrollToRef(calculadoraRef)}
-              className="inline-block text-black font-semibold py-2 px-6 rounded shadow transition hover:brightness-90 bg-accent"
+              className="inline-block text-black font-semibold py-2 px-4 md:px-6 rounded shadow transition hover:brightness-90 bg-accent text-sm md:text-base"
             >
               CALCULA TUS GANANCIAS
             </button>
@@ -52,7 +54,7 @@ export default function LandingPage() {
             <img
               src="/img/darmax-logo.png"
               alt="Logo Darmax"
-              className="w-[300px] h-auto object-contain"
+              className="w-[200px] sm:w-[250px] md:w-[300px] lg:w-[250px] h-auto object-contain"
             />
           </div>
         </div>
