80561f069c661752ad6a4d2dc3cf6df97c2ecd04
GarkarAXO <e.axel12@gmail.com>
2025-12-16 14:11:50 -0600

    update(seo)

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -17,7 +17,7 @@ export default function LandingPage() {
     <>
       <Helmet>
         <title>
-          Darmax | Purificadoras de Agua, Máquinas Vending y Negocios de Limpieza
+          Darmax Agua | Purificadoras, Vending y Limpieza para Negocios
         </title>
       </Helmet>
 
@@ -33,15 +33,12 @@ export default function LandingPage() {
         <div className="flex flex-col lg:flex-row items-center justify-center gap-4 md:gap-2 w-full max-w-7xl mx-auto lg:h-full">
           <div className="max-w-xl text-center md:text-left flex flex-col items-center md:items-start">
             <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7x2 font-black tracking-tight leading-tight mt-0 md:mt-1 mb-1 md:mb-3">
-              Emprende tu{" "}
               <span style={{ color: '#ffff00' }}>
-                Negocio
-              </span>
-              
+                Negocios
+              </span> de Agua Purificada, Vending y Limpieza
             </h1>
             <p className="text-base md:text-lg lg:text-lg text-white leading-relaxed mb-6 md:mb-8">
-              Da el primer paso a tu libertad financiera y <br />
-              lleva tu emprendimiento al siguiente nivel con 
+              Con Darmax, inicia tu emprendimiento con purificadoras de agua, máquinas vending 24/7 y productos de limpieza de alta calidad.
             </p>
             <button
               onClick={() => scrollToRef(calculadoraRef)}
